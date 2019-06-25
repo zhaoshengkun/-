7a1bbc1b347c75c0c3eccbe1de056d3689c68f94
bcptdtptp@123 <1551454546@qq.com>
2019-06-26 00:06:36 +0800

    大傻逼

---
+++ src/components/view/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="body" style="">
+  <div class="hello">
+    
+    {{msg}}
+   
+    
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+      msg: '大傻逼'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello{
+  border: 1px solid green;
+  width: 60%;
+  height: 400px;
+  margin: 0 auto;
+  background-color:#fff;
+}
+.body{
+width:100%;height:500px;background-color:#ccc;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 
 Vue.use(Router)
 
@@ -9,7 +9,14 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      meta: {
+        hideInMenu: true,
+        title: '首页',
+        notCache: true,
+        icon: 'md-home'
+      },
+      component: () => import('@/components/view/HelloWorld')
+      
     }
   ]
 })
